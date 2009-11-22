a8369bb17a1253a2d9eebcedc72cca663d5e5c48
Kevin Baribeau <kbaribeau@Kevin-Baribeaus-MacBook-Pro.local>
2009-11-21 23:31:53 -0500

    * remove a bit of code not under test

--- jsVim.js
+++ jsVim.js
@@ -1,31 +1,24 @@
 var INSERT_MODE = 0;
 var COMMAND_MODE = 1;
 var mode = INSERT_MODE;
-var instrumentTextarea = function(textarea) {
-	var textareaKeydown = function(e) {
-		if (mode == COMMAND_MODE) {
-			e.preventDefault(); // not under test
-			if (e.keyCode == 73) { // 73 == 'i'
-					mode = INSERT_MODE;
-			}
-			else {
-				textarea.value = '';
-			}
+var textareaKeydown = function(e) {
+	if (mode == COMMAND_MODE) {
+		e.preventDefault(); // not under test
+		if (e.keyCode == 73) { // 73 == 'i'
+				mode = INSERT_MODE;
 		}
-		else if (mode == INSERT_MODE) {
-			// ESC, 27
-			// 91, [
-			if (e.ctrlKey && e.keyCode == 91 || e.keyCode == 27) {
-					mode = COMMAND_MODE;
-			}
+		else {
+			textarea.value = '';
 		}
-	};
+	}
+	else if (mode == INSERT_MODE) {
+		// ESC, 27
+		// 91, [
+		if (e.ctrlKey && e.keyCode == 91 || e.keyCode == 27) {
+				mode = COMMAND_MODE;
+		}
+	}
+};
 
-	textarea.addEventListener("keydown", textareaKeydown, true);
-}
-
-var textareas = document.getElementsByTagName("textarea");
-for (var i = 0; i < textareas.length; i++) 
-{
-	   instrumentTextarea(textareas[i]);
-}
+var textarea = document.getElementsByTagName("textarea")[0];
+textarea.addEventListener("keydown", textareaKeydown, true);
